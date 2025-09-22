07632ca8a6a1af89d7a2a93be8b1ffc73f0be241
unknown <admin@Lab-223-pc-06.LABSFC.COM>
2025-09-21 23:05:22 -0700

    update

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const express = require("express")
+const express = require("express");
+const ConnectDb = require("./config/db");
+const dotenv = require('dotenv')
+dotenv.config()
 const app = express()
-
+const PORT=process.env.PORT||5000;
+ConnectDb();
 app.get( "/",(rq,res)=>{
-    res.send("hello express")
+    res.send("hello world")
+})
+app.listen(PORT,()=>{
+    console.log(`Server is runing on port ${PORT}`)
 })
 
-
-
-
-
-
-app.listen(3000)
 
